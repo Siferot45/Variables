1319900bf6fdde380c49be858e56d8899c077f41
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-10-03 11:19:14 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+complited_homework = 12
+
+all_time_spent = 1.5
+
+course_name = "Python"
+
+time_one_task = all_time_spent / complited_homework
+
+print(f"Курс: {course_name}, всего задач: {complited_homework}, затрачено часов: {all_time_spent}, среднее время выполнения {time_one_task:.3f} часа.")
